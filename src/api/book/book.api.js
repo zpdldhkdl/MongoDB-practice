beb518f88777ab0b84be1bb49c2948bc0d36b7a2
DESKTOP-A43AVN4\kyong <lklklnm@daum.net>
2020-09-22 23:15:51 +0900

    add findAll

--- src/api/book/book.api.js
+++ src/api/book/book.api.js
@@ -3,6 +3,10 @@ const bookSchema = require('../../models/book');
 exports.createBook = async(req, res) => {
     const { title, author } = req.body;
 
+    if(!title || !author) {
+        return res.status(400).send(`title or author empty`);
+    }
+
     const book = new bookSchema({
         title: title,
         author: author,
@@ -10,10 +14,23 @@ exports.createBook = async(req, res) => {
 
     try {
         await book.save();
+        res.redirect('/');
     }
     catch(e) {
         res.status(500).send(e);
     }
+}
 
-    res.redirect('/');
+exports.getAllBook = async(req, res) => {
+    const book = bookSchema;
+
+    try {
+        const dbResponse = await book.find({}, {_id: 0, __v: 0});
+        if(dbResponse) {
+            res.status(200).json(dbResponse);
+        }
+    }
+    catch(e) {
+        res.status(500).send(e);
+    }
 }
--- src/api/book/index.js
+++ src/api/book/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bookRouter = express.Router();
 const {
     createBook,
+    getAllBook,
 } = require('./book.api');
 
+bookRouter.get('/books', getAllBook);
 bookRouter.post('/books', createBook);
 
 module.exports = bookRouter;
