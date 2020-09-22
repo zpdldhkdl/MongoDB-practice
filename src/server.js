62250cc1e4d42bb2201c2452e4e5b578b1fb6ec4
DESKTOP-A43AVN4\kyong <lklklnm@daum.net>
2020-09-22 21:38:04 +0900

    fix server

--- src/server.js
+++ src/server.js
@@ -2,32 +2,17 @@ const config = require('./config');
 const express = require('express');
 const app = express();
 const router = express.Router();
-const bodyParser = require('body-parser');
-const book = require('./models/book');
 const db = require('./lib/mongodb');
+const apis = require('./api');
 
-/*app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);*/
+app.engine('html', require('ejs').renderFile);
 
-app.post('/', (req, res) => {
-    let book1 = new book();
-    book1.title = 'test';
-    book1.author = 'test2';
+router.use('/api', apis);
 
-    book1.save((err) => {
-        if(err) {
-            console.error(err);
-            res.json({result: 0});
-            return;
-        }
+const htmlRouter = require('./router/index')(app);
 
-        res.json({result: 1});
-    });
-});
-
-//const router = require('./router/index')(app);
-
-const server = app.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
     console.log(`express server has started on port ${config.PORT}`);
 });
